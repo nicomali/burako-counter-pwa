73213669492e162b8c6cb55b673b3c9dfd5da6af
Nico Mali <yonicomali@gmail.com>
2025-12-26 08:02:22 -0300

    Fix PWA refs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "./",
   plugins: [
+    react(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg"],
